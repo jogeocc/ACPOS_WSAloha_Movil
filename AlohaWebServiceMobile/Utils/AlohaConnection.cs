fd9d4b6be664be8aaae99162f22790430c26c327
Sebastian dzul <sebassdc@hotmail.com>
2022-08-05 12:58:30 -0500

    Cambios de aloha service mobile

--- AlohaWebServiceMobile/Utils/AlohaConnection.cs
+++ AlohaWebServiceMobile/Utils/AlohaConnection.cs
@@ -16,7 +16,9 @@ namespace AlohaWebServiceMobile.Utils
     {
 
         private SdkFunctions _sdkFunctions = new SdkFunctions();
+
         private IIberFuncs23 xFunction;
+
         public ResponseAloha login(int IdTerm, int IdEmpleado)
         {
             ResponseAloha responseAloha = new ResponseAloha();
@@ -40,22 +42,7 @@ namespace AlohaWebServiceMobile.Utils
             }
             return responseAloha;
         }
-        public ResponseAloha logout(int IdTerm)
-        {
-            ResponseAloha response = new ResponseAloha();
-            try
-            {
-                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
-                xFunction.LogOut(IdTerm);
-                response.Estado = true;
-            }
-            catch (Exception ex)
-            {
-                response.Estado = false;
-                App.logger.Error("Error al salie con el usuario tal", ex);
-            }
-            return response;
-        }
+
         public ResponseAloha ClockIn(int IdTerm, int IdJobCode)
         {
             ResponseAloha response = new ResponseAloha();
@@ -72,21 +59,7 @@ namespace AlohaWebServiceMobile.Utils
             }
             return response;
         }
-        public bool ClockOut(int IdTerm, double tips, double DeclaredCash)
-        {
-            bool IsSuccess = false;
-            try
-            {
-                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
-                xFunction.PerformCheckout(IdTerm, DeclaredCash);
-                xFunction.ClockOut(IdTerm, tips);
-            }
-            catch (Exception ex)
-            {
-                App.logger.Error("Error en Clock Out", ex);
-            }
-            return IsSuccess;
-        }
+
         public ResponseAloha OpenTable(int IdTerm, int idNumMesa, string NombreMesa, int NumInvitados)
         {
             ResponseAloha responseAloha = new ResponseAloha();
@@ -104,6 +77,7 @@ namespace AlohaWebServiceMobile.Utils
             }
             return responseAloha;
         }
+
         public ResponseAloha OpenTab(int IdTerm, int idNumMesa, string NombreMesa, int NumInvitados)
         {
             ResponseAloha responseAloha = new ResponseAloha();
@@ -122,6 +96,43 @@ namespace AlohaWebServiceMobile.Utils
             }
             return responseAloha;
         }
+
+        public ResponseAloha OpenCheck(int IdTerm, int IdMesaInterno)
+        {
+            ResponseAloha responseAloha = new ResponseAloha();
+
+            try
+            {
+                int idChequeInterno = xFunction.AddCheck(IdTerm, IdMesaInterno);
+                responseAloha.Codigo = (int)CodigosError.NO_ERROR;
+                responseAloha.mensaje = $"Cuenta abierda con id {idChequeInterno}";
+                responseAloha.idMesa = idChequeInterno;
+            }
+            catch (Exception ex)
+            {
+                App.logger.Error($"Error al abrir cheque", ex);
+            }
+            return responseAloha;
+        }
+
+        public ResponseAloha CloseCheck(int IdTerm, int IdCheckInterno)
+        {
+            ResponseAloha responseAloha = new ResponseAloha();
+            try
+            {
+                xFunction.CloseCheck(IdTerm, IdCheckInterno);
+                responseAloha.Codigo = (int)CodigosError.NO_ERROR;
+                responseAloha.mensaje = $"Cuenta Cerrada con id ";
+            }
+            catch (Exception ex)
+            {
+                responseAloha.mensaje = $"Error al cerrar cuenta con id ";
+                App.logger.Error($"Error al cerrar cheque", ex);
+            }
+            return responseAloha;
+
+        }
+
         public ResponseAloha CloseTabTable(int IdTerm, int IdMesaInterno)
         {
             ResponseAloha responseAloha = new ResponseAloha();
@@ -141,42 +152,43 @@ namespace AlohaWebServiceMobile.Utils
             return responseAloha;
         }
 
-
-
-        public ResponseAloha OpenCheck(int IdTerm, int IdMesaInterno)
+        public ResponseAloha logout(int IdTerm)
         {
-            ResponseAloha responseAloha = new ResponseAloha();
-
+            ResponseAloha response = new ResponseAloha();
             try
             {
-                int idChequeInterno = xFunction.AddCheck(IdTerm, IdMesaInterno);
-                responseAloha.Codigo = (int)CodigosError.NO_ERROR;
-                responseAloha.mensaje = $"Cuenta abierda con id {idChequeInterno}";
-                responseAloha.idMesa = idChequeInterno;
+                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
+                xFunction.LogOut(IdTerm);
+                response.Estado = true;
             }
             catch (Exception ex)
             {
-                App.logger.Error($"Error al abrir cheque", ex);
+                response.Estado = false;
+                App.logger.Error("Error al salie con el usuario tal", ex);
             }
-            return responseAloha;
+            return response;
         }
-        public ResponseAloha CloseCheck(int IdTerm, int IdCheckInterno)
+
+        public bool ClockOut(int IdTerm, double tips, double DeclaredCash)
         {
-            ResponseAloha responseAloha = new ResponseAloha();
+            bool IsSuccess = false;
             try
             {
-                xFunction.CloseCheck(IdTerm, IdCheckInterno);
-                responseAloha.Codigo = (int)CodigosError.NO_ERROR;
-                responseAloha.mensaje = $"Cuenta Cerrada con id ";
+                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
+                xFunction.PerformCheckout(IdTerm, DeclaredCash);
+                xFunction.ClockOut(IdTerm, tips);
             }
             catch (Exception ex)
             {
-                responseAloha.mensaje = $"Error al cerrar cuenta con id ";
-                App.logger.Error($"Error al cerrar cheque", ex);
+                App.logger.Error("Error en Clock Out", ex);
             }
-            return responseAloha;
-
+            return IsSuccess;
         }
+
+
+
+
+
         public ResponseAloha AddItem(int IdTerm, int IdCheck, int IdItem, double Amount, int IdMod, double AmountMod)
         {
             ResponseAloha responseAloha = new ResponseAloha();
@@ -246,68 +258,18 @@ namespace AlohaWebServiceMobile.Utils
         }
 
         //FUNCIONES DE CONTROL DE DATOS
-        private bool IsAlreadyClockIn(int IdEmpleado)
-        {
-            bool IsClocked = false;
-            try
-            {
-                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
-                IIberDepot depot = AlohaSdkFactory.GetIberDepotInstance();
-                IberEnum EnumEmpleados = depot.FindObjectFromId((int)COMEnums.INTERNAL_EMPLOYEES, IdEmpleado);
-                IberObject empleado = EnumEmpleados.First();
-                var clock = empleado.GetBoolVal("CLOCKED_IN");
-                IsClocked = empleado.GetBoolVal("CLOCKED_IN") == 1 ? true : false;
-            }
-            catch (Exception ex)
-            {
-                App.logger.Error("", ex);
-            }
-            return IsClocked;
 
-        }
-        private string NombreEmpleado(int IdEmpleado)
+        public ResponseAloha ListTables(int IdEmpleado)
         {
-            string nombre = "";
-            try
-            {
-                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
-                IIberDepot depot = AlohaSdkFactory.GetIberDepotInstance();
-                IberEnum EnumEmpleados = depot.FindObjectFromId((int)COMEnums.INTERNAL_EMPLOYEES, IdEmpleado);
-                IberObject empleado = EnumEmpleados.First();
-                nombre = empleado.GetStringVal("NICKNAME");
-            }
-            catch (Exception ex)
-            {
-                App.logger.Error("Error recuperando nombre del empleado", ex);
-            }
+            ResponseAloha responseAloha = new ResponseAloha();
+            responseAloha.Codigo = (int)CodigosError.NO_ERROR;
+            responseAloha.mensaje = "Mesas recuperadas con exito";
+            responseAloha.Nombre_Empleado = NombreEmpleado(IdEmpleado);
+            responseAloha.mesas_empleado = RecuperarMesas(IdEmpleado);
+            return responseAloha;
+        }
 
-            return nombre;
-        }
-        private List<int> IdsJobsEmpleado(int IdEmpleado)
-        {
-            List<int> ListaJobs = new List<int>();
-            try
-            {
-                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
-                IIberDepot depot = AlohaSdkFactory.GetIberDepotInstance();
-                IberEnum EnumEmpleados = depot.FindObjectFromId((int)COMEnums.INTERNAL_EMPLOYEES, IdEmpleado);
-                IberObject empleado = EnumEmpleados.First();
-                for (int i = 0; i < 10; i++)
-                {
-                    string NameJob = $"JOBCODE{(i + 1)}";
-                    int job = empleado.GetLongVal(NameJob);
-                    if (job > 0)
-                    {
-                        ListaJobs.Add(job);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                App.logger.Error($"Error al recuperar los jobs del empelado {IdEmpleado}", ex);
-            }
-            return ListaJobs;
-        }
+
 
         private List<MesaEmpleado> RecuperarMesas(int IdEmpleado)
         {
@@ -466,15 +428,71 @@ namespace AlohaWebServiceMobile.Utils
             return ListaMesas;
         }
 
-        public ResponseAloha ListTables(int IdEmpleado)
+        private List<int> IdsJobsEmpleado(int IdEmpleado)
         {
-            ResponseAloha responseAloha = new ResponseAloha();
-            responseAloha.Codigo = (int)CodigosError.NO_ERROR;
-            responseAloha.mensaje = "Mesas recuperadas con exito";
-            responseAloha.Nombre_Empleado = NombreEmpleado(IdEmpleado);
-            responseAloha.mesas_empleado = RecuperarMesas(IdEmpleado);
-            return responseAloha;
+            List<int> ListaJobs = new List<int>();
+            try
+            {
+                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
+                IIberDepot depot = AlohaSdkFactory.GetIberDepotInstance();
+                IberEnum EnumEmpleados = depot.FindObjectFromId((int)COMEnums.INTERNAL_EMPLOYEES, IdEmpleado);
+                IberObject empleado = EnumEmpleados.First();
+                for (int i = 0; i < 10; i++)
+                {
+                    string NameJob = $"JOBCODE{(i + 1)}";
+                    int job = empleado.GetLongVal(NameJob);
+                    if (job > 0)
+                    {
+                        ListaJobs.Add(job);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                App.logger.Error($"Error al recuperar los jobs del empelado {IdEmpleado}", ex);
+            }
+            return ListaJobs;
         }
+
+        private string NombreEmpleado(int IdEmpleado)
+        {
+            string nombre = "";
+            try
+            {
+                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
+                IIberDepot depot = AlohaSdkFactory.GetIberDepotInstance();
+                IberEnum EnumEmpleados = depot.FindObjectFromId((int)COMEnums.INTERNAL_EMPLOYEES, IdEmpleado);
+                IberObject empleado = EnumEmpleados.First();
+                nombre = empleado.GetStringVal("NICKNAME");
+            }
+            catch (Exception ex)
+            {
+                App.logger.Error("Error recuperando nombre del empleado", ex);
+            }
+
+            return nombre;
+        }
+
+        private bool IsAlreadyClockIn(int IdEmpleado)
+        {
+            bool IsClocked = false;
+            try
+            {
+                xFunction = AlohaSdkFactory.GetIberFuncs23Instance();
+                IIberDepot depot = AlohaSdkFactory.GetIberDepotInstance();
+                IberEnum EnumEmpleados = depot.FindObjectFromId((int)COMEnums.INTERNAL_EMPLOYEES, IdEmpleado);
+                IberObject empleado = EnumEmpleados.First();
+                var clock = empleado.GetBoolVal("CLOCKED_IN");
+                IsClocked = empleado.GetBoolVal("CLOCKED_IN") == 1 ? true : false;
+            }
+            catch (Exception ex)
+            {
+                App.logger.Error("", ex);
+            }
+            return IsClocked;
+
+        }
+
     }
 
 }
