da8151861ad545b4bd5091796d88043a1fbfd4f8
sebas25dc <sdzul@acnti.com.mx>
2022-09-12 17:27:40 -0500

    Recuperando generalidades para reconocer productos por PESO

---
+++ AlohaLibrary/Modelos/QTYPRICE.cs
@@ -0,0 +1,23 @@
+﻿using AlohaLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlohaLibrary.Modelos
+{
+    public class QTYPRICE
+    {
+        public int ID { get; set; }
+        public int OWNERID { get; set; }
+        public int USERNUMBER { get; set; }
+        public int ITEMID { get; set; }
+        public int TAREID { get; set; }
+        public string UNITNAME { get; set; }
+        public int DECIMALS { get; set; }
+        public double UNITPRICE { get; set; }
+        public TipoLogicoALH AFFECTINVT { get; set; }
+        public TipoLogicoALH ITEMQTY { get; set; }
+    }
+}
