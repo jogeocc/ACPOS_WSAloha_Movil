b92578b7e7405884e148b9b4fe3d6d81a2577de5
sebas25dc <sdzul@acnti.com.mx>
2022-09-13 09:10:01 -0500

    Cambios nuevos para encontrar items de tipo kilaje

---
+++ AlohaLibrary/Implementaciones/QTYPRICEServicio.cs
@@ -0,0 +1,65 @@
+﻿using AlohaLibrary.Contexto;
+using AlohaLibrary.Infraestrutura;
+using AlohaLibrary.Interfaces;
+using AlohaLibrary.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlohaLibrary.Implementaciones
+{
+    public class QTYPRICEServicio : ServicioBaseALH<QTYPRICE>, IQTYPRICE
+    {
+        public QTYPRICEServicio(AplicacionBdContextoALH contexto) : base(contexto)
+        {
+        }
+
+        public override List<QTYPRICE> GetAll()
+        {
+            List<QTYPRICE> List = new List<QTYPRICE>();
+            string query = $"SELECT * FROM QTYPRICE";
+            DataSet ds = new DataSet();
+            EjecutarConsulta(query).Fill(ds, "QTYPRICE");
+            DataTable tabla = ds.Tables["QTYPRICE"];
+            foreach (DataRow item in tabla.Rows)
+            {
+                QTYPRICE priceItem = new QTYPRICE();
+
+                var Props = priceItem.GetType().GetProperties().ToList();
+                foreach (PropertyInfo prop in Props)
+                {
+                    if (prop.PropertyType == typeof(double))
+                    {
+                        //VALORES DOUBLE
+                        double.TryParse(item[prop.Name].ToString(), out double result);
+                        prop.SetValue(priceItem, result);
+                    }
+                    else
+                    {
+                        if (int.TryParse(item[prop.Name].ToString(), out int value))
+                        {
+                            //ENTEROS
+                            prop.SetValue(priceItem, value);
+                        }
+                        else if ((item[prop.Name].ToString().ToUpper() == "Y" || item[prop.Name].ToString().ToUpper() == "N"))
+                        {
+                            //BOOLEANO TIPO ALOHA
+                            prop.SetValue(priceItem, item[prop.Name].ToString().ToUpper() == "Y");
+                        }
+                        else
+                        {
+                            //CADENAS
+                            prop.SetValue(priceItem, item[prop.Name].ToString());
+                        }
+                    }
+                }
+                List.Add(priceItem);
+            }
+            return List;
+        }
+    }
+}
