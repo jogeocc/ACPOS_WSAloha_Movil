385206f19621bae1d419f40cb080514cab08d0ea
sebas25dc <sdzul@acnti.com.mx>
2022-09-29 11:20:24 -0500

    .

--- AlohaWebServiceMobile/Utils/AlohaConnection.cs
+++ AlohaWebServiceMobile/Utils/AlohaConnection.cs
@@ -899,6 +899,7 @@ namespace AlohaWebServiceMobile.Utils
             try
             {
                 IIberPrinter iberPrinter = AlohaSdkFactory.GetIberPrinterInstance();
+                string info = iberPrinter.GetAllPrinters();
                 iberPrinter.PrintStream($"< PRINT >< PRINTER >{impresora}</ PRINTER >< COMMANDS >< PRINTLINE > Hello XML World </ PRINTLINE >< RED > 1 </ RED >< PRINTLEFTRIGHT >< LEFT > Hello </ LEFT >< RIGHT > World </ RIGHT ></ PRINTLEFTRIGHT >< RED > 0 </ RED >< PRINTFILLED > *</ PRINTFILLED >< LINEFEED > 3 </ LINEFEED >< BITMAP >< PATH > test.bmp </ PATH >< SIZE > 1 </ SIZE >< JUST > 0 </ JUST ></ BITMAP >< CUT > PARTIAL </ CUT ></ COMMANDS ></ PRINT > ");
             }
             catch (Exception ex)
