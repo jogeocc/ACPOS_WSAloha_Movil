ba4bab48574ad36bd7a39b40dc32c70adeb836b6
sebas25dc <sdzul@acnti.com.mx>
2022-09-30 12:33:04 -0500

    Cambio sin hilos

--- AlohaWebServiceMobile/Utils/AlohaConnection.cs
+++ AlohaWebServiceMobile/Utils/AlohaConnection.cs
@@ -302,55 +302,61 @@ namespace AlohaWebServiceMobile.Utils
         }
 
 
+        //public ResponseAloha AddItem(RequestAddItem requestAddItem)
+        //{
+        //    ResponseAloha responseAloha = new ResponseAloha();
+
+        //    bool error = false;
+        //    while (!error)
+        //    {
+        //    }
+
+
+        //    return responseAloha;
+        //}
+
         public ResponseAloha AddItem(RequestAddItem requestAddItem)
         {
             ResponseAloha responseAloha = new ResponseAloha();
 
-            bool error = false;
-            while (!error)
+            try
             {
-                try
+
+                VerificarIber();
+                LoginInterno(requestAddItem.IdTerm, requestAddItem.IdEmpleado);
+
+                int idEntry = xFunction.BeginItem(requestAddItem.IdTerm, requestAddItem.IdCheck, requestAddItem.item.IdItem, "", requestAddItem.item.Amount);
+                #region modificadores
+                foreach (var mod in requestAddItem.item.Mods)
                 {
-
-                    VerificarIber();
-                    LoginInterno(requestAddItem.IdTerm, requestAddItem.IdEmpleado);
-
-                    int idEntry = xFunction.BeginItem(requestAddItem.IdTerm, requestAddItem.IdCheck, requestAddItem.item.IdItem, "", requestAddItem.item.Amount);
-                    #region modificadores
-                    foreach (var mod in requestAddItem.item.Mods)
-                    {
-                        xFunction.ModItem(requestAddItem.IdTerm, idEntry, mod.IdMod, "", mod.Amount, mod.ModCode);
-                    }
-                    #endregion
-                    xFunction.EndItem(requestAddItem.IdTerm);
-                    if (!string.IsNullOrEmpty(requestAddItem.item.SpecialMessage) || !string.IsNullOrEmpty(requestAddItem.item.Unidad_Medida))
-                    {
-                        string Mensaje = "";
-
-                        Mensaje += requestAddItem.item.Cantidad_Peso > 0 ? requestAddItem.item.Cantidad_Peso.ToString() : "";
-
-                        Mensaje += !string.IsNullOrEmpty(requestAddItem.item.Unidad_Medida) ? requestAddItem.item.Unidad_Medida : "";
-
-                        Mensaje += !string.IsNullOrEmpty(requestAddItem.item.SpecialMessage) ? $" {requestAddItem.item.SpecialMessage}" : "";
-
-                        xFunction.ApplySpecialMessage(requestAddItem.IdTerm, requestAddItem.IdCheck, idEntry, Mensaje);
-                    }
-                    responseAloha.Codigo = (int)CodigosError.NO_ERROR;
-                    responseAloha.mensaje = "Producto insertado con exito";
-                    error = true;
-                    LogoutInterno(requestAddItem.IdTerm);
+                    xFunction.ModItem(requestAddItem.IdTerm, idEntry, mod.IdMod, "", mod.Amount, mod.ModCode);
                 }
-                catch (Exception ex)
+                #endregion
+                xFunction.EndItem(requestAddItem.IdTerm);
+                if (!string.IsNullOrEmpty(requestAddItem.item.SpecialMessage) || !string.IsNullOrEmpty(requestAddItem.item.Unidad_Medida))
                 {
-                    responseAloha.Codigo = (int)CodigosError.ERROR;
-                    responseAloha.Estado = false;
-                    responseAloha.mensaje = $"Error al agregar item {(ErroresAloha.MensajeMobile(ex.Message))}";
-                    App.logger.Error("Error al agregar item", ex);
-                    LogoutInterno(requestAddItem.IdTerm);
-                };
+                    string Mensaje = "";
+
+                    Mensaje += requestAddItem.item.Cantidad_Peso > 0 ? requestAddItem.item.Cantidad_Peso.ToString() : "";
+
+                    Mensaje += !string.IsNullOrEmpty(requestAddItem.item.Unidad_Medida) ? requestAddItem.item.Unidad_Medida : "";
+
+                    Mensaje += !string.IsNullOrEmpty(requestAddItem.item.SpecialMessage) ? $" {requestAddItem.item.SpecialMessage}" : "";
+
+                    xFunction.ApplySpecialMessage(requestAddItem.IdTerm, requestAddItem.IdCheck, idEntry, Mensaje);
+                }
+                responseAloha.Codigo = (int)CodigosError.NO_ERROR;
+                responseAloha.mensaje = "Producto insertado con exito";
+                LogoutInterno(requestAddItem.IdTerm);
             }
-
-
+            catch (Exception ex)
+            {
+                responseAloha.Codigo = (int)CodigosError.ERROR;
+                responseAloha.Estado = false;
+                responseAloha.mensaje = $"Error al agregar item {(ErroresAloha.MensajeMobile(ex.Message))}";
+                App.logger.Error("Error al agregar item", ex);
+                LogoutInterno(requestAddItem.IdTerm);
+            };
             return responseAloha;
         }
 
