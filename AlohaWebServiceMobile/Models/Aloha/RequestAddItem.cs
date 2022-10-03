80ed9176f48420347d5fe578fe2f4bf246009d4e
sebas25dc <sdzul@acnti.com.mx>
2022-10-03 12:07:24 -0500

    .

--- AlohaWebServiceMobile/Models/Aloha/RequestAddItem.cs
+++ AlohaWebServiceMobile/Models/Aloha/RequestAddItem.cs
@@ -11,7 +11,7 @@ namespace AlohaWebServiceMobile.Models.Aloha
         public int IdEmpleado { get; set; }
         public int IdTerm { get; set; }
         public int IdCheck { get; set; }
-        public List<ItemAloha> item { get; set; }
+        public List<ItemAloha> item { get; set; } = new List<ItemAloha>();
         public ItemAloha _item { get; set; }
     }
 
