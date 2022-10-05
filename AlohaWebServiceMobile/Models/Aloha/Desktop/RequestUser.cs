d26beb55c41582f430054644e1d84ea1125c2694
sebas25dc <sdzul@acnti.com.mx>
2022-10-05 10:47:41 -0500

    Documenacion generada

--- AlohaWebServiceMobile/Models/Aloha/Desktop/RequestUser.cs
+++ AlohaWebServiceMobile/Models/Aloha/Desktop/RequestUser.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AlohaWebServiceMobile.Models.Aloha.Desktop
 {
+    /// <summary>
+    /// Modelo de peticion de Datos para liberacion de usuarios
+    /// </summary>
     public class RequestUser
     {
+        /// <summary>
+        /// Gets or sets the identifier user.
+        /// </summary>
+        /// <value>
+        /// Id del usuario que va a ser liberado.
+        /// </value>
         public int IdUser { get; set; }
     }
 }
