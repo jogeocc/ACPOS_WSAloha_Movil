06690055c9364310ed44b917f50c3932d35b81a6
sergio sebastian dzul can <SebasSDC@hotmail.com>
2022-10-14 14:48:04 -0600

    Realizando pruebas

--- AlohaWebServiceMobile/Utils/AlohaConnection.cs
+++ AlohaWebServiceMobile/Utils/AlohaConnection.cs
@@ -99,6 +99,7 @@ namespace AlohaWebServiceMobile.Utils
                 VerificarIber();
                 Encolamiento();
                 App.IsBusy = true;
+                App.logger.Info($"Creando mesa {idNumMesa} de usuario {idEmpleado}");
                 LoginInterno(IdTerm, idEmpleado);
                 int IdMesaInterno = xFunction.AddTable(IdTerm, 0, idNumMesa, NombreMesa, NumInvitados);
                 responseAloha.idMesa = IdMesaInterno;
@@ -159,7 +160,7 @@ namespace AlohaWebServiceMobile.Utils
                 VerificarIber();
                 Encolamiento();
                 App.IsBusy = true;
-                LoginInterno(IdTerm, idEmpleado);
+                //LoginInterno(IdTerm, idEmpleado);
                 var Mesas = RecuperarMesas(idEmpleado);
                 if (Mesas.Exists(M => M.Id == IdMesaInterno))
                 {
@@ -169,14 +170,15 @@ namespace AlohaWebServiceMobile.Utils
                 }
                 else
                 {
-                    responseAloha.idMesa = xFunction.AddCheck(IdTerm, IdMesaInterno);
+                    App.logger.Info($"Cheque no creado, creando otra vez");
+                    //responseAloha.idMesa = xFunction.AddCheck(IdTerm, IdMesaInterno);
                 }
 
                 int idChequeInterno =
                 responseAloha.Codigo = (int)CodigosError.NO_ERROR;
                 responseAloha.mensaje = $"Cuenta abierda con id {idChequeInterno}";
                 //responseAloha.idMesa = idChequeInterno;
-                LogoutInterno(IdTerm);
+                //LogoutInterno(IdTerm);
             }
             catch (Exception ex)
             {
