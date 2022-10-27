c816471581dacdaf9c315ba16d78426845e3065b
sebas25dc <sdzul@acnti.com.mx>
2022-10-27 15:53:20 -0500

    Cambios por espaciado de datos

--- AlohaWebServiceMobile/Utils/AlohaConnection.cs
+++ AlohaWebServiceMobile/Utils/AlohaConnection.cs
@@ -1070,7 +1070,7 @@ namespace AlohaWebServiceMobile.Utils
                     {
                         articulo.SubArticulos.Add(new Articulo
                         {
-                            Nombre = mod.Name,
+                            Nombre = mod.Name.Trim().Insert(0, ">"),
                             Importe = decimal.Parse(mod.Price.ToString())
                         });
 
