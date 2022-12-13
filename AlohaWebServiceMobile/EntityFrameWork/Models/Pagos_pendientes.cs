4ddb9f58aacb4708d7cce38d0b7295199ad03336
sebas25dc <sdzul@acnti.com.mx>
2022-12-13 14:38:34 -0600

    Aplicando cambios y correcciones para el sistema

---
+++ AlohaWebServiceMobile/EntityFrameWork/Models/Pagos_pendientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlohaWebServiceMobile.EntityFrameWork.Models
+{
+    public class Pagos_pendientes
+    {
+        [Key]
+        public int id { get; set; }
+        public int infoPago { get; set; }
+        public int IdEmpleado { get; set; }
+        public string Fecha { get; set; }
+        public int CheckID { get; set; }
+        public string NombreMesa { get; set; }
+        public double Pago { get; set; }
+        public double Tip { get; set; }
+        public string ReferenciaUnica { get; set; }
+
+    }
+}
