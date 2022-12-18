980beeaa34f7052a49ed7757f304e96627d472d4
sebas25dc <sdzul@acnti.com.mx>
2022-12-18 14:30:00 -0600

    Corrigiendo actualizar

--- AlohaWebServiceMobile/Utils/AlohaConnection.cs
+++ AlohaWebServiceMobile/Utils/AlohaConnection.cs
@@ -1277,6 +1277,7 @@ namespace AlohaWebServiceMobile.Utils
                         Pagos_pendientes PagoPendiente = db.Pagos_pendientes.ToList().FindLast(PP => PP.id == requestPagoPendiente.id);
                         PagoPendiente.infoPago = requestPagoPendiente.infoPago;
                         responseAloha.IdPagoPendiente = PagoPendiente.id;
+                        db.SaveChanges();
 
                     }
 
