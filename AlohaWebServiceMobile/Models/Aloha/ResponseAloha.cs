eca8ca6f3521f8f0ea87b9ce8add74784af6f8f8
sebas25dc <sdzul@acnti.com.mx>
2023-01-16 15:00:02 -0600

    .

--- AlohaWebServiceMobile/Models/Aloha/ResponseAloha.cs
+++ AlohaWebServiceMobile/Models/Aloha/ResponseAloha.cs
@@ -22,7 +22,5 @@ namespace AlohaWebServiceMobile.Models.Aloha
         public string NombreMesa { get; set; }
         public int IdPagoPendiente { get; set; }
         public string SG_REFERENCE { get; set; } = "";
-
-        public 
     }
 }
