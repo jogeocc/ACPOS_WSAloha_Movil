2c72a0395e9696c19f138ffbfe93f21fa3c64abd
Sebas22dc <sebassdc@hotmail.com>
2023-05-05 11:25:16 -0600

    Correccion tipo de datos para la tabla

--- AlohaWebServiceMobile/EntityFrameWork/Models/Producto_Pedido_Espera.cs
+++ AlohaWebServiceMobile/EntityFrameWork/Models/Producto_Pedido_Espera.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace AlohaWebServiceMobile.EntityFrameWork.Models
 {
     public class Producto_Pedido_Espera
     {
+        [Key]
+        public int ID { get; set; }
         public int IdTerminal { get; set; }
         public int IdEmpleado { get; set; }
         public int NumberCheck { get; set; }
@@ -17,6 +20,6 @@ namespace AlohaWebServiceMobile.EntityFrameWork.Models
         public int IdOrderMode { get; set; }
         public DateTime HoldStart { get; set; }
         public DateTime HoldEnd { get; set; }
-        public string HoldMinutes { get; set; } 
+        public string HoldMinutes { get; set; }
     }
 }
