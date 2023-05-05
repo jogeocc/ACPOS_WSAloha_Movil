47544ad5433275bae6d290ff36b92cf72b71ca99
Sebas22dc <sebassdc@hotmail.com>
2023-05-05 09:10:25 -0600

    Creando clase para sistema de productos en espera

---
+++ AlohaWebServiceMobile/EntityFrameWork/Models/Producto_Pedido_Espera.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlohaWebServiceMobile.EntityFrameWork.Models
+{
+    public class Producto_Pedido_Espera
+    {
+
+    }
+}
