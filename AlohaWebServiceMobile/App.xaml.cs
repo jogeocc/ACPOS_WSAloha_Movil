a05163bc9e3999b8ac56e732773c2dceaf3e918c
Sebas22dc <sebassdc@hotmail.com>
2023-09-14 16:56:37 -0600

    Version 18 actualizada

--- AlohaWebServiceMobile/App.xaml.cs
+++ AlohaWebServiceMobile/App.xaml.cs
@@ -24,7 +24,7 @@ namespace AlohaWebServiceMobile
 
     public partial class App : Application
     {
-        public static string Version = "Versión 17";
+        public static string Version = "Versión 18";
         public static readonly ILog logger = LogManager.GetLogger("Aloha_vapiano");
         public bool iniciar = false;
         public bool IsError = false;
