788b3b2d73b62d8f2ad0c6bd249f32b5a9d5fa51
Sebas22dc <sebassdc@hotmail.com>
2023-09-15 14:24:34 -0600

    Correccion aplicada

--- AlohaWebServiceMobile/Utils/EstructurarData.cs
+++ AlohaWebServiceMobile/Utils/EstructurarData.cs
@@ -408,7 +408,7 @@ namespace AlohaWebServiceMobile.Utils
 
             if (IdsPaneles.Contains(Panel.id_panel))
             {
-                return item;
+                return null;
             }
 
             IdsPaneles.Add(Panel.id_panel);
