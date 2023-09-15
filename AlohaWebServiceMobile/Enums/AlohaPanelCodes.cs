10d5796b037a68809f4ac705ffa5bc2bdfde7b38
Sebas22dc <sebassdc@hotmail.com>
2023-09-15 16:22:21 -0600

    Cambios

--- AlohaWebServiceMobile/Enums/AlohaPanelCodes.cs
+++ AlohaWebServiceMobile/Enums/AlohaPanelCodes.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace AlohaWebServiceMobile.Enums
 {
+
+
+
     public enum AlohaPanelCodes
     {
         BOTON_PANEL = 199,
         BOTON_ITEM = 195,
+
+
+
+        //
         BOTON_SMART_ITEM = 194,
+        ///FUNCIONAMIENTO
+        ///PARAMETRO 1 DESCONOCIDO
+        ///PARAMETRO 2 NUMERO DEL ITEM
+        ///PARAMETRO 3 PRECIO DEL ITEM SI ES USADO EL BUTTON PRICE
+        ///PARAMETRO 4 EL PRICE LEVEL SI ES USADO PRICE LEVEL.
+        ///PARAMTRO 5 ES EL PRICE METHOD QUE PUEDE SER ITEM PRICE, BUTTON PRICE O PRICE LEVEL
+        //ESTANDAR DE NUMERACIONES DE ALOHA POS NCR
+        //BUTTON PRICE ES = 0
+
+
+
+        //ITEM PRICE ES = 1
+        //PRICE LEVEL = 2
+        //
     }
 }
