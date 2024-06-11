d9cc95171dff09f25238835b700d7bc1ccca5a78
jogeocc <jogeochanc@gmail.com>
2024-06-11 12:43:55 -0600

    Cambio para ASKPrice y ASKDesc

--- WindowsServiceAlohaMobile/Utils/EstructurarData.cs
+++ WindowsServiceAlohaMobile/Utils/EstructurarData.cs
@@ -405,8 +405,8 @@ namespace WindowsServiceAlohaMobile.Utils
                                     ItemMOD.Impuesto_1 = itemDBF.TAXID;
                                     ItemMOD.Impuesto_2 = itemDBF.TAXID2;
                                     ItemMOD.Impuesto_3 = itemDBF.VTAXID;
-                                   // ItemMOD.AskDesc = itemDBF.ASKDESC == TipoLogicoALH.Y;
-                                   // ItemMOD.AskPrice = itemDBF.ASKPRICE == TipoLogicoALH.Y;
+                                    ItemMOD.AskDesc = itemDBF.ASKDESC == TipoLogicoALH.Y;
+                                    ItemMOD.AskPrice = itemDBF.ASKPRICE == TipoLogicoALH.Y;
 
                                     int condicion = ListaMods[i].PRMETHOD;
 
