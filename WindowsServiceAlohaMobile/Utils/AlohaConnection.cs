e14231d59a3b76c12547c3e55d5190167e1b2f60
sebas22dc <sebassdc@hotmail.com>
2024-06-18 11:27:22 -0600

    Cambio para insertar productos que solicitan nombre o no

--- WindowsServiceAlohaMobile/Utils/AlohaConnection.cs
+++ WindowsServiceAlohaMobile/Utils/AlohaConnection.cs
@@ -419,15 +419,17 @@ namespace WindowsServiceAlohaMobile.Utils
                 foreach (ItemAloha item in requestAddItem.item)
                 {
 
+                    var NameItem = ACPOS_SERVICE_MOBILE.itemsAskDesc.Any(itm => itm.ID == item.IdItem) ? item.DescItem : "";
+
                     int IdEntryBase = 0;
 
                     if (item.NumSilla > 0)
                     {
-                        IdEntryBase = xFunction.BeginPivotSeatItem(requestAddItem.IdTerm, requestAddItem.IdCheck, item.IdItem, item.DescItem, item.Amount, item.NumSilla);
+                        IdEntryBase = xFunction.BeginPivotSeatItem(requestAddItem.IdTerm, requestAddItem.IdCheck, item.IdItem, NameItem, item.Amount, item.NumSilla);
                     }
                     else
                     {
-                        IdEntryBase = xFunction.BeginItem(requestAddItem.IdTerm, requestAddItem.IdCheck, item.IdItem, item.DescItem, item.Amount);
+                        IdEntryBase = xFunction.BeginItem(requestAddItem.IdTerm, requestAddItem.IdCheck, item.IdItem, NameItem, item.Amount);
                     }
 
                     IdsEntryes.Add(IdEntryBase);
@@ -922,13 +924,16 @@ namespace WindowsServiceAlohaMobile.Utils
 
                     int IdEntryBase = 0;
 
+                    var NameItem = ACPOS_SERVICE_MOBILE.itemsAskDesc.Any(itm => itm.ID == item.IdItem) ? item.DescItem : "";
+
+
                     if (item.NumSilla > 0)
                     {
-                        IdEntryBase = xFunction.BeginPivotSeatItem(requestHoldCheck.IdTerm, requestHoldCheck.IdCheck, item.IdItem, "", item.Amount, item.NumSilla);
+                        IdEntryBase = xFunction.BeginPivotSeatItem(requestHoldCheck.IdTerm, requestHoldCheck.IdCheck, item.IdItem, NameItem, item.Amount, item.NumSilla);
                     }
                     else
                     {
-                        IdEntryBase = xFunction.BeginItem(requestHoldCheck.IdTerm, requestHoldCheck.IdCheck, item.IdItem, "", item.Amount);
+                        IdEntryBase = xFunction.BeginItem(requestHoldCheck.IdTerm, requestHoldCheck.IdCheck, item.IdItem, NameItem, item.Amount);
                     }
                     IdsEntryes.Add(IdEntryBase);
                     #region SECCION DE MODIFICADORES.
